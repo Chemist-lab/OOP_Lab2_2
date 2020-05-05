3996f29510204094fdd2df580808dacaca6a3d27
Chemist-lab <60918051+Chemist-lab@users.noreply.github.com>
2020-05-05 20:22:23 +0300

    2

---
+++ 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double res = 0;
+            Console.WriteLine("a: ");
+            float a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("b: ");
+            float b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("dx: ");
+            float dx = float.Parse(Console.ReadLine());
+            double x = a + dx;
+            Console.WriteLine("  x       y=f(x)\n");
+            while (x < b)
+            {
+                res = (Math.Log10(x))/(4*x+13);
+                x += x + dx;
+                
+                Console.WriteLine(Math.Round(x, 5) + "  |  "+ Math.Round(res, 5));
+            }
+        }
+    }
+}
